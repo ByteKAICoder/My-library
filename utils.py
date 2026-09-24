9ec4848bdb37326a357d790da890a162e4e90cb4
ByteKAICoder <ksu06052008@gmail.com>
2026-09-24 15:59:03 +0500

    Create utils.py
    Добавлена функция загрузки данных из JSON-файла

---
+++ utils.py
@@ -0,0 +1,6 @@
+def load_books():
+    """Загружаем книги из файла"""
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    return []
